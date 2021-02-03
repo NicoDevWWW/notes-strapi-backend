65341733ff630e731c29913b523e32443d178549
Nicolas SQUEREN <nicolas.squeren@gmail.com>
2021-02-03 10:36:39 +0100

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fd3a3f469e75df53d067e11fab75c82d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '870ffd3e-d55d-4266-b3b1-f7362a7a572e'
+};
