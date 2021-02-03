96d5c870682ca7d1a5742f65421d402d2efbb790
Nicolas SQUEREN <nicolas.squeren@gmail.com>
2021-02-03 12:06:57 +0100

    Add Note api

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -4,17 +4,17 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: env('DATABASE_URI')
-        // host: env('DATABASE_HOST', 'strapi-cluster.gewqw.mongodb.net'),
-        // srv: env.bool('DATABASE_SRV', true),
-        // port: env.int('DATABASE_PORT', 27017),
-        // database: env('DATABASE_NAME', 'backend'),
-        // username: env('DATABASE_USERNAME', 'strapi'),
-        // password: env('DATABASE_PASSWORD', 'K4CFM3Im4gNUIebK'),
+        // uri: env('DATABASE_URI')
+        host: env('DATABASE_HOST', 'strapi-cluster.gewqw.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'backend'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'K4CFM3Im4gNUIebK'),
       },
       options: {
-        //authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: true  //env.bool('DATABASE_SSL', true),
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl:  env.bool('DATABASE_SSL', true),
       },
     },
   },
